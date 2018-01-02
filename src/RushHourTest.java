7ed2e82b694055bbf2f9a25fa4497f77b1e1127d
pierre-tholoniat <pierre.tholoniat@polytechnique.edu>
2018-01-02 10:58:21 +0100

    interversion  x y

--- src/RushHourTest.java
+++ src/RushHourTest.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class RushHourTest {
 
 	public static void main(String[] args) {
-		State test1 = new State(new File("C:/Users/Pierre/github/Rusho/test/GameP18.txt"));
+		State test1 = new State(new File("C:/Users/Pierre/github/Rusho/test/GameP01.txt"));
 		test1.display();
 	}
 
