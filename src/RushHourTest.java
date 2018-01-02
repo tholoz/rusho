1698a12d1f5a25ffce726f49faa36630d1be8c25
tholoz <32133147+tholoz@users.noreply.github.com>
2018-01-02 11:45:18 +0100

    Update RushHourTest.java

--- src/RushHourTest.java
+++ src/RushHourTest.java
@@ -3,10 +3,6 @@ import java.io.File;
 public class RushHourTest {
 
 	public static void main(String[] args) {
-		int[][] grid = new int [2][3];
-		System.out.println(grid.toString());
-		
-		
 		State test1 = new State(new File("C:/Users/Pierre/github/Rusho/test/GameP01.txt"));
 		test1.display();
 	}
